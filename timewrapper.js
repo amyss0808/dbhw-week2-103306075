c143d53b267c551312c50ae4bc6320c1f7acdecf
amyss0808 <amychung0808@gmail.com>
2016-03-16 22:45:38 +0800

    0316

---
+++ timewrapper.js
@@ -0,0 +1,22 @@
+function minus() {
+  if (document.getElementById("num")) {
+    var curr = parseInt(document.getElementById("num").innerHTML);
+  }
+
+  if (curr && curr <= 60) {
+    if (curr == 1) {
+      document.getElementById("num").innerHTML = 60;
+    }else if (curr <= 10 ) {
+      document.getElementById("num").innerHTML = "0" + (curr - 1);
+    } else {
+      document.getElementById("num").innerHTML = curr - 1;
+    }
+  } else {
+    document.getElementById("num").innerHTML = 60;
+  }
+
+  setTimeout(function() {
+    minus();
+  }, 1000);
+};
+minus();
